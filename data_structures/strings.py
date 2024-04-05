fd6fd2f4d210b7d0ad7d7348409b93c1d8756a73
varsha@mac <varshakirani@gmail.com>
2024-04-05 14:47:34 +0200

    Add: anagram checker

--- data_structures/strings.py
+++ data_structures/strings.py
@@ -16,3 +16,29 @@ def string_reverser(our_string):
         reversed_str += our_string[i]
     
     return reversed_str
+
+
+def anagram_checker(str1, str2):
+
+    """
+    Check if the input strings are anagrams of each other
+    An anagram is a word (or phrase) that is formed by rearranging the letters of another word (or phrase).
+    For example:
+        "rat" is an anagram of "art"
+        "alert" is an anagram of "alter"
+        "Slot machines" is an anagram of "Cash lost in me"
+
+    Args:
+       str1(string),str2(string): Strings to be checked
+    Returns:
+       bool: Indicates whether strings are anagrams
+    """
+
+    
+    str1 = sorted(str1.lower().replace(" ", ""))
+    str2 = sorted(str2.lower().replace(" ", ""))
+    
+    if str1 == str2:
+        return(True)
+    else:
+        return(False)
--- data_structures/tests/test_strings.py
+++ data_structures/tests/test_strings.py
@@ -1,4 +1,4 @@
-from data_structures.strings import string_reverser
+from data_structures.strings import string_reverser, anagram_checker
 class TestStrings:
 
     def test_string_reverser(self):
@@ -7,3 +7,9 @@ class TestStrings:
         assert '3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')
 
     
+    def test_anagram_checker(self):
+        assert not anagram_checker('water','waiter')
+        assert anagram_checker('Dormitory','Dirty room')
+        assert anagram_checker('Slot machines', 'Cash lost in me')
+        assert not (anagram_checker('A gentleman','Elegant men'))
+        assert anagram_checker('Time and tide wait for no man','Notified madman into water')
